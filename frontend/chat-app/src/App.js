5fe7b42f6da6260097f558028cecf5be768433cc
szymptom <bpodkolzin@kse.org.ua>
2024-09-29 18:17:24 +0300

    fixed bugs & provide client joining room logic

--- frontend/chat-app/src/App.js
+++ frontend/chat-app/src/App.js
@@ -2,9 +2,37 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import WaitingRoom from './components/waitingRoom';
-
+import { useState } from 'react';
+import * as signalR from '@microsoft/signalr';
 
 function App() {
+  const [connection, setConnection] = useState();
+
+  const joinChatRoom = async (username, chatroom) => {
+    try
+    {
+      const newConnection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:5238/chat")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+      newConnection.on("JoinChatRoom", (username, message) => {
+        console.log(`${username}: ${message}`);
+      
+      });
+      
+      await newConnection.start();
+
+      await newConnection.invoke("JoinSpecificChat", { Username: username, ChatRoom: chatroom });
+
+      setConnection(newConnection);
+    } 
+    catch (e)
+    {
+      console.log(e);
+    }
+  };
+
   return (
     <div>
       <main>
@@ -14,7 +42,7 @@ function App() {
               <h1 className='font-weight-light'>Welcome to the club, buddy</h1>
             </Col>
           </Row>
-          <WaitingRoom/>
+          <WaitingRoom joinChatRoom={joinChatRoom} />
         </Container>
       </main>
     </div>
